72445a99be62bdd6c86692d34dae3438dc5d29ef
cryptobfund <cryptobfund@gmail.com>
2020-07-19 15:59:39 +0300

    step 4 test commit from IDE

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,11 @@ namespace First\Project\Cli;
 use function cli\line;
 use function cli\prompt;
 
+//функция запуска проекта
 function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
 }
