6eb59842d058983cc136793da21fa9a2e182b704
cryptobfund <cryptobfund@gmail.com>
2020-07-26 18:42:48 +0300

    refactoring 2

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -1,28 +1,46 @@
 <?php
 
-namespace First\Project\Games\Even;
+namespace First\Project\Games\Calc;
 
 use function First\Project\Engine\gameStart;
 
-const GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no"'; //общее описание и вопрос игры
+const GAME_DESCRIPTION = 'What is the result of the expression?'; //общее описание и вопрос игры
 const MIN_RAND = 0; //мнимальное для генерации числа вопроса значение
 const MAX_RAND = 10; //максимальное для генерации числа вопроса значение
+const OPERATIONS = ['+', '-', '*']; //массив возможных математических операций в задаче игры
 
 function generateRoundData()
 {
-    $num = rand(MIN_RAND, MAX_RAND);
+    $firstNum = rand(MIN_RAND, MAX_RAND);
+    $secondNum = rand(MIN_RAND, MAX_RAND);
+    $operation = OPERATIONS[rand(0, count(OPERATIONS) - 1)];
+    try {
+        $answer = (string) calculate($firstNum, $secondNum, $operation);
+    } catch (\Exception $e) {
+        echo "\n", "Program error. " , $e->getMessage(), "\n";
+        exit;
+    }
     return [
-        'answer' => isEven($num) ? 'yes' : 'no',
-        'question' => (string) $num
+        'answer' => $answer,
+        'question' => "{$firstNum} {$operation} {$secondNum}"
     ];
 }
-
 function start()
 {
-    gameStart(fn() => generateRoundData(), GAME_DESCRIPTION);
+    gameStart(function () {
+        return generateRoundData();
+    }, GAME_DESCRIPTION);
 }
-
-function isEven($num)
+function calculate($firstNum, $secondNum, $operation)
 {
-    return $num % 2 === 0;
+    switch ($operation) {
+        case '*':
+            return $firstNum * $secondNum;
+        case '-':
+            return $firstNum - $secondNum;
+        case '+':
+            return $firstNum + $secondNum;
+        default:
+            throw new \Exception('Operator not found');
+    }
 }
